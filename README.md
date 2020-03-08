6f23dc8c9b21b65bacb529b281a6a58422529563
fhernan22 <fhern103@fiu.edu>
2020-03-07 22:46:09 -0500

    Updated server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Name:   Fidel Hernandez
 PID:    6045594
 
+I spent most of the time trying to figure out how to use the code provided by the instructor.
+
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import confundo
+#!/usr/bin/env python3
+
 import sys
 import socket
 import signal
@@ -6,26 +7,37 @@ import os
 from _thread import *
 import threading
 import struct
-import argparse
 
 HOST = '0.0.0.0'
-
-# Example how to use the provided confundo.Header class
-sampleInput = b'\x00\x00\x00*\x00\x00\x00\x00\x00\x00\x00\x02sample-buffer'
-print(sampleInput)
-print(sampleInput[0:12])
-
-
-pkt = confundo.header.Header()
-pkt.decode(sampleInput[0:12])
+establishedConnections = 0
 
 def receiveSignal(signalNumber, frame):
     print('Exiting server with signanl number:', signalNumber)
     sys.exit(0)
 
-def main():
-    print(pkt.seqNum, pkt.ackNum, pkt.connId, pkt.isAck, pkt.isSyn, pkt.isFin)
 
+def clientThreaded(conn, addr, connNum):
+    conn.settimeout(10)
+
+    with open(os.path.join(os.getcwd(), sys.argv[2], str(connNum) + '.file'), 'wb') as f:
+
+        try:
+            data = conn.recv(1024)
+
+            while data:
+                f.write(data)
+                data = conn.recv(1024)
+
+            conn.close()
+
+        except socket.timeout as e:
+            sys.stderr.write('ERROR: {}'.format(e))
+            f.write('ERROR'.encode())
+            sys.exit(1)
+
+
+
+def main():
     try:
         if int(sys.argv[1]) > 1023:
             PORT = int(sys.argv[1])
@@ -43,22 +55,38 @@ def main():
     if not os.path.exists(sys.argv[2]):
         os.mkdir(sys.argv[2], mode = 0o777) 
 
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    conn = confundo.Socket(sock)
-
     try:
-        remote = socket.getaddrinfo(HOST, PORT, family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    except (socket.error, OverflowError) as e:
-        sys.stderr.write("ERROR: Invalid hostname or port (%s)\n" % e)
-        sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as e:
+        sys.stderr.write('ERROR: {}\n'.format(e))
         sys.exit(1)
 
-    (family, type, proto, canonname, sockaddr) = remote[0]
+    try:
+        sock.bind((HOST, PORT))
+        print("Server running on " + str(HOST) +  ":" + str(PORT))
+
+        sock.listen(5)
+
+        while True:
+            conn, addr = sock.accept()
+            global establishedConnections
+            establishedConnections += 1
+            print(establishedConnections)
+
+            print('Connection to :', addr[0], ':', addr[1])
 
 
-    conn.connect(sockaddr)
+            start_new_thread(clientThreaded, (conn, addr, establishedConnections,)) 
+
+        sock.close()
+
+    except socket.error as e:
+        sys.stderr.write('ERROR:{}\n'.format(e))
+        sys.exit(1)
+    
+    except KeyboardInterrupt as e:
+        sys.stderr.write('Shutting down server...\n')
+        sys.exit(0)
 
 
 
